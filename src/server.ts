7b22ecc74e88900d3f3a5b90e65a3bbde6e8b697
Ajay <ajay20101995@gmail.com>
2024-02-21 23:06:40 +0530

    logging unhandled rejections and exceptions and return 404 as JSON

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,21 @@ app.use(bodyParser.json());
 app.use('/admin', adminRoutes);
 app.use('/user', userRoutes);
 
+app.all('*', async (req: express.Request, res: express.Response) => res.status(404).json({
+    statusText: 'FAIL', statusValue: 404, message: 'Requested url is not available..',
+}));
+
+
+process.on('uncaughtException', (error: any) => {
+    console.log('----- Uncaught exception -----', error);
+    return;
+});
+
+process.on('unhandledRejection', (error: any) => {
+    console.log('----- Unhandled Rejection at -----', error);
+    return;
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
